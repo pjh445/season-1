eb69fc7b5f02afc07040ddfc487ff1514b1d2c89
jeonghwa,park <pjh445@gmail.com>
2024-08-22 11:20:17 +0900

    Update season.js

--- js/season.js
+++ js/season.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded" , ()=> {   
     //링크가 이동된 후에 해당 페이지로 ?p0 인수를 전달한다
     const para = new URLSearchParams(window.location.search); //?p0 (쿼리스트링부분)
+    console.log( para.toString() ); //?p0
     
 
     //페이지가 로드 된후 실행
@@ -9,9 +10,9 @@ document.addEventListener("DOMContentLoaded" , ()=> {
     menu.addEventListener("load" , ()=>{
         //부모페이지에서 자식 아이프레임의 콘텐츠에 접근가능해야한다.
         const nav = menu.contentDocument.querySelectorAll("nav a");// 각각각 메뉴 <a href="spring">
-        nav.forEach( i => {
+        /* nav.forEach( i => {
                 i.classList.add('act');
-        });
+        }); */
     });
 
 
