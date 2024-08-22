3eaae406c24076d7b24b0e5e098c0506aec53ff7
jeonghwa,park <pjh445@gmail.com>
2024-08-22 10:57:31 +0900

    Update season.js

--- js/season.js
+++ js/season.js
@@ -1,11 +1,12 @@
-document.addEventListener("DOMContentLoaded" , ()=> {
-
-    const menu = document.getElementById('menu');  //<iframe id="menu">
-    const nav = menu.contentDocument.querySelectorAll("nav a");// 각각각 메뉴 <a href="spring">
+document.addEventListener("DOMContentLoaded" , ()=> {   
+    
 
     //페이지가 로드 된후 실행
+    const menu = document.getElementById('menu');  //<iframe id="menu">
+    
     menu.addEventListener("load" , ()=>{
         //부모페이지에서 자식 아이프레임의 콘텐츠에 접근가능해야한다.
+        const nav = menu.contentDocument.querySelectorAll("nav a");// 각각각 메뉴 <a href="spring">
         nav.forEach( i => {
                 i.classList.add('act');
         });
